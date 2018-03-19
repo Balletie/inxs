3425ec324175c91c56d1f7ced49c0e0c50ef02b7
Balletie <skip_meesie@hotmail.com>
2018-03-19 17:35:30 +0100

    Remove lxml smart_prefix install from setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
 from sys import version_info
 
 try:
@@ -25,69 +23,6 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-
-class lxmlInstall():
-    @staticmethod
-    def check_lxml():
-        try:
-            from lxml import etree  # noqa: F401
-        except ImportError:
-            return False
-        else:
-            return True
-
-    @staticmethod
-    def check_smart_prefix():
-        # FIXME for some reason this check doesn't work
-        #       in a repl no exception is thrown either
-        from lxml import etree
-        evaluator = etree.XPathEvaluator(etree.Element('x'))
-        try:
-            evaluator('/*', smart_prefix=True)
-        except TypeError as e:
-            if "got an unexpected keyword argument 'smart_prefix'" in str(e):
-                return False
-            raise
-        else:
-            return True
-
-    def ensure_lxml_with_smart_prefix(self):
-        if not self.check_lxml():
-            self.install_lxml()
-        elif not self.check_smart_prefix():
-            pip(['uninstall', '--yes', '-v', 'lxml'])
-            self.install_lxml()
-
-    def install_lxml(self):
-        # FIXME use verbosity levels from the invokation of this file
-        # FIXME check success of subprocesses and abort in case of failure
-        try:
-            import cython  # noqa: F401
-        except ImportError:
-            cython_was_installed = False
-            pip(['install', '-v', 'cython'])
-        else:
-            cython_was_installed = True
-
-        pip(['install', '-v',
-             'https://github.com/funkyfuture/lxml/tarball/smart_xpath#egg=lxml'])
-
-        if not cython_was_installed:
-            pip(['uninstall', '--yes', '-v', 'cython'])
-
-
-class Develop(lxmlInstall, develop):
-    def run(self):
-        self.ensure_lxml_with_smart_prefix()
-        super().run()
-
-
-class Install(lxmlInstall, install):
-    def run(self):
-        self.ensure_lxml_with_smart_prefix()
-        self.do_egg_install()
-
-
 setup(
     name='inxs',
     version=VERSION,
@@ -99,7 +34,6 @@ setup(
     packages=['inxs'],
     package_dir={'inxs': 'inxs'},
     include_package_data=True,
-    cmdclass={'develop': Develop, 'install': Install},
     install_requires=('cssselect', 'dependency_injection'),
     license="ISC license",
     zip_safe=False,
